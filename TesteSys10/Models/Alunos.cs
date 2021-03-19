697b43879b1bd14412a5073286b0c1b40a0af09d
Gustavo Caldeira <gusteitiom@hotmail.com>
2021-03-19 18:01:17 -0300

    Commit Inicial com classes

---
+++ TesteSys10/Models/Alunos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteSys10.Models
+{
+    public class Alunos
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public float Nota { get; set; }
+    }
+}
