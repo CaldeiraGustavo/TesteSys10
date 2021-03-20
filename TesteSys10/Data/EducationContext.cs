4619d7f5eba3931c5cce04ee8c3e9c4db41a0363
Gustavo Caldeira <gusteitiom@hotmail.com>
2021-03-19 21:12:11 -0300

    Conexão com o banco de dados

---
+++ TesteSys10/Data/EducationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteSys10.Models;
+
+namespace TesteSys10.Data
+{
+    public class EducationContext : DbContext 
+    {
+        public EducationContext (DbContextOptions<EducationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Alunos> Alunos { get; set; }
+        public DbSet<Escola> Escola { get; set; }
+        public DbSet<Turma> Turma { get; set; }
+    }
+}
--- TesteSys10/Startup.cs
+++ TesteSys10/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TesteSys10.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace TesteSys10
 {
@@ -27,6 +22,7 @@ namespace TesteSys10
         {
 
             services.AddControllers();
+            services.AddDbContext<EducationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("EducationContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
